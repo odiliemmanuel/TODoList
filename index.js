c24a5f0829ed465205aeeaee2f8f12443b9c59d9
odiliemmanuel <odiliejeh@gmail.com>
2026-03-26 20:15:17 +0100

    tasks

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
+document.getElementById("addBtn").addEventListener("click", add);
+document.getElementById("search").addEventListener("click", search);
 
+
+const userInput = document.getElementById("input");
+const searchInput = document.getElementById("inputSearch");
+const display = document.getElementById("output");
+
+
+function add(){
+
+    
+
+    const inputValue = userInput.value.trim();
+
+    if(inputValue !== ""){
+      
+
+        const inputWrapper = document.createElement("div");
+        inputWrapper.classList.add("input-wrapper");
+
+        const inputText = document.createElement("span");
+        inputText.classList.add("input-text");
+        inputText.textContent = inputValue;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("deleteBtn");
+
+    
+        deleteButton.addEventListener("click", function() {
+            inputWrapper.remove();
+        });
+
+        inputWrapper.appendChild(inputText);
+        inputWrapper.appendChild(deleteButton);
+
+        display.appendChild(inputWrapper);
+
+        userInput.value = "";
+    }
+
+}
+
+
+
+
+function search(){
+
+    const searchValue = searchInput.value.trim().toLowerCase();
+    const inputWrappers = display.getElementsByClassName("input-wrapper");
+
+     for(let i = 0; i < inputWrappers.length; i++){
+        const inputText = inputWrappers[i].querySelector(".input-text").textContent.toLowerCase();
+
+        if(inputText.includes(searchValue)){
+            inputWrappers[i].style.backgroundColor = "lightgray";
+            
+
+        }
+         searchInput.value = "";
+       
+    }
+     console.log(searchValue);
+    
+
+   
+}
